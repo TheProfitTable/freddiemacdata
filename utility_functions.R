783b41d5917554297179c90e691013ac89bae9b6
Fernando Flores <floresf82@gmail.com>
2017-11-20 18:08:08 -0300

    Issue #8: Get the list of contracts with incomplete performance history in the dataset

--- utility_functions.R
+++ utility_functions.R
@@ -37,4 +37,34 @@ trans_int_month_date <- function(date_var) {
 }
 
 
+#' @title get_trimmed_contracts
+#' @description get the list of contracts with incomplete performance history considering the last available 
+#'              month in the dataset
+#' @param data a data frame that contains at least contract_key and pointintime_month
+#'
+#' @return named list of contract_key with continuity issues
+#' @export
+#'
+#' @examples
+#' contract_list <- get_trimmed_contracts(df)
+#' contract_list$contract_key
+get_trimmed_contracts <- function(data) {
+  by_last_month <-
+    data %>% 
+    group_by(contract_key) %>% 
+    summarize(max_pointintime_month = max(pointintime_month)) %>% 
+    ungroup()
+  
+  last_month_in_df <- max(by_last_month$max_pointintime_month)
+  
+  ## Note: pull() is available on dplyr since v0.7.0
+  contract_list <- 
+    by_last_month %>% 
+    filter(max_pointintime_month < last_month_in_df) %>% 
+    pull(contract_key) %>% 
+    list("contract_key" = .)
+  
+  return(contract_list)
+}
+
 
