dc0c6f019b4effd0ece8f4638c9b2377a3693a50
Fernando Flores <floresf82@gmail.com>
2017-11-24 17:56:48 -0300

    Issue #8: Style modifications about functions for missing values (issue #12)

--- utility_functions.R
+++ utility_functions.R
@@ -9,12 +9,16 @@
 
 
 # Constants used in functions
+# =======================================
 
 # Used on workaround for missing values columns (issue #12)
 UNKNOWN_STRING <- "unknown"
 UNKNOWN_INT <- -1
 UNKNOWN_DATE <- as.Date("2222-12-31")
 
+
+# Functions
+# =======================================
 
 #' @title last_day
 #' @description calculate last day of the month given a date.  
@@ -179,7 +183,7 @@ complete_history <- function(data) {
 
 #' @title impute_na
 #' @description Impute NA in some columns of the performance history data with a default value
-#' @param data a data frame that contains the processed columns:
+#' @param data a data frame that contains the columns to impute:
 #'             - \code{months_arrears}
 #'             - \code{fico}
 #'             - \code{cltv}
@@ -198,21 +202,22 @@ complete_history <- function(data) {
 #' 
 impute_na <- function(data) {
   # Column details:
-  #
-  # months_arrears - int  - includes 0 usually
-  # fico           - int  - ...
-  # cltv           - int  - ...
-  # dti            - int  - ...
-  # default_flag   - num  - includes 0 usually
-  # default_month  - Date - constant value "2222-01-01" (at the moment)
-  # fico_bin       - chr  - ...
-  # cltv_bin       - chr  - ...
-  # dti_bin        - chr  - ...
+  # --------------
+  # Name           - Type - Remarks
+  # months_arrears - int  - value can include 0
+  # fico           - int  - value doesn't include 0
+  # cltv           - int  - value doesn't include 0
+  # dti            - int  - value doesn't include 0
+  # default_flag   - num  - value can include 0
+  # default_month  - Date - usually "2222-01-01" (can be a valid date in the past)
+  # fico_bin       - chr  - bin expressed as a string
+  # cltv_bin       - chr  - bin expressed as a string
+  # dti_bin        - chr  - bin expressed as a string
   
+  # Workaround for missing values (issue #12)
   # TODO(floresfdev): Generalize to unobserved cases
   data_processed <- 
     data %>% 
-    # Workaround for missing values (issue #12)
     replace_na(list(months_arrears = as.integer(UNKNOWN_INT),
                     fico = as.integer(UNKNOWN_INT), 
                     cltv = as.integer(UNKNOWN_INT),
@@ -229,7 +234,7 @@ impute_na <- function(data) {
 
 #' @title reset_na
 #' @description Reinstate NA from a default value in some columns of the performance history data
-#' @param data a data frame that contains the processed columns:
+#' @param data a data frame that contains the imputed columns:
 #'             - \code{months_arrears}
 #'             - \code{fico}
 #'             - \code{cltv}
@@ -247,10 +252,10 @@ impute_na <- function(data) {
 #' df_na_reset <- reset_na(df_imputed)
 #' 
 reset_na <- function(data) {
+  # Workaround for missing values (issue #12)
   # TODO(floresfdev): Generalize to unobserved values
   data_processed <- 
     data %>% 
-    # Workaround for missing values (issue #12)
     mutate(months_arrears = replace(months_arrears, which(months_arrears == UNKNOWN_INT), NA),
            fico = replace(fico, which(fico == UNKNOWN_INT), NA),
            cltv = replace(cltv, which(cltv == UNKNOWN_INT), NA),
